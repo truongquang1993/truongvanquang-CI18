7cb93c60036469d6895070422b668094d30be264
unknown <truongquanggt@gmail.com>
2019-05-04 19:20:31 +0700

    initial commit

---
+++ 03 Rectangle/src/Rectangle.java
@@ -0,0 +1,38 @@
+public class Rectangle {
+    Vector2D rectanglePosition;
+    double x, y;
+    int height, width;
+
+
+    public Rectangle(double x, double y, int width, int height) {
+        this(new Vector2D(x, y), width, height);
+    }
+
+    public Rectangle(Vector2D rectanglePosition, int width, int height) {
+        this.rectanglePosition = rectanglePosition;
+        this.width = width;
+        this.height = height;
+    }
+
+    public boolean intersects (Rectangle other) {
+
+        Rectangle rec = new Rectangle(rectanglePosition,width,height);
+
+        if (rec.rectanglePosition.x <= other.rectanglePosition.x && other.rectanglePosition.x <= rec.rectanglePosition.x+ rec.width &&
+                rec.rectanglePosition.y <= other.rectanglePosition.y && other.rectanglePosition.y <= rec.rectanglePosition.y + rec.height) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Rectangle rec1 = new Rectangle(30,35,20,20);
+        Rectangle rec2 = new Rectangle(40,40,5,5);
+        Rectangle rec3 = new Rectangle(10,10,5,5);
+
+        System.out.println(rec1.intersects(rec2));
+        System.out.println(rec1.intersects(rec3));
+        System.out.println(rec2.intersects(rec3));
+    }
+}
---
+++ 03 Rectangle/src/Vector2D.java
@@ -0,0 +1,64 @@
+import tklibs.Mathx;
+
+public class Vector2D {
+    public double x;
+    public double y;
+    public double rate;
+
+    public Vector2D() {
+        this(0, 0);
+    }
+
+    public Vector2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void add(double x, double y) {
+        this.x += x;
+        this.y += y;
+    }
+
+    public void subtract(double x, double y) {
+        this.x -= x;
+        this.y -= y;
+    }
+
+    public void scale(double rate) {
+        this.x *= rate; // this.x = this.x * rate
+        this.y *= rate;
+    }
+
+    public void set(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector2D clone() {
+        return new Vector2D(this.x, this.y);
+    }
+
+    public double getLength() {
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public void setLength(double length) {
+        double oldLength = this.getLength();
+        if(oldLength != 0) {
+            this.x = this.x * length / oldLength;
+            this.y = this.y * length / oldLength;
+        }
+    }
+
+    public double getAngle() {
+        return Math.atan(y/x);
+    }
+
+    public void setAngle(double angle) {
+        double length = this.getLength();
+        if(length != 0){
+            this.x = length * Math.cos(angle);
+            this.y = length * Math.sin(angle);
+        }
+    }
+}
